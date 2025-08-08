1aaf413ba7b5225105f390ed7b9ab4c7b560a464
Ashutosh Tiwari <ashutoshtiwari200128@gmail.com>
2025-08-08 19:50:33 +0530

    testing deploy

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>Testing production</h1>
+        <h2>Hello world</h2>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
